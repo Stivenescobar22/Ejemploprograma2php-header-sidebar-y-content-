7f3895b16319ae23167cb248adfad8887837039d
stiven escobar galeano <estibenes@gmail.com>
2019-11-24 13:21:32 -0500

    se implementan las etiquetas basicas de php

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE HTML>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        Ejemplo pagina Responsive Design
+    </title>
+    <link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body>
+  
+  <div id="pagewrap">
+
+      <div id="header">
+          <div id="info">
+                <h1>Black Friday</h1>
+                <p>Aprovecha nuestras ofertas de fin de semana</p>
+          </div>
+
+          <div id="logo">
+                <img class = "imagen1" src="imagenes/blackfriday.webp">
+                
+          </div>
+      
+        
+      </div>
+
+     <div id="content">
+       <h2>Contenido</h2>
+       <p>parrafo1</p>
+       <p>parrafo2</p>
+       <p>parrafo3</p>
+       <p>parrafo4</p>
+       <p>parrafo5</p>
+       <p>parrafo6</p>
+     </div>
+
+     <div id="sidebar">
+        <h3>Sidebar</h3>
+        <p>texto1</p>
+        <p>texto2</p>
+        <p>texto3</p>
+        <p>texto4</p>
+       
+     </div>
+
+     <div id="footer">
+         <h4>Footer</h4>
+
+     </div>
+
+  </div>
+  
+</body>
+</html>
